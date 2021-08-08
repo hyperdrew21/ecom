1ee44fc51cd05ca6b32c6661c2ded06cd95cea57
hyperdrew21 <angryfiredragon@gmail.com>
2021-08-08 17:33:34 -0400

    added two fields to the form

--- src/components/auth/signinform.js
+++ src/components/auth/signinform.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
+import { FormInput } from '../formFields';
+
 class SignInForm extends Component {
     render() {
         const { className } = this.props;
         return (
             <form className={`${className} sign-in-form`}>
-                sign in
+                <Field className="sign-in-form_email" 
+                type="Email"
+                title="Email"
+                placeholder="Email" 
+                name="Email"
+                component={FormInput}/>
+                 <Field className="sign-in-form_password" 
+                type="password"
+                title="Password"
+                placeholder="password" 
+                name="Password"
+                component={FormInput}/>
+                
             </form>
         )
     }
---
+++ src/components/formFields.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class FormInput extends Component {
+    render() {
+        const { className, title, type, placeholder, input } = this.props;
+        return (
+            <div className={`${className} form-input`}>
+                <label className="form-input_label">
+                    {title}
+                </label>
+                <input className="form-input_input" 
+                type={type} placeholder={placeholder} {...input}/>
+            </div>
+                )
+    }
+}

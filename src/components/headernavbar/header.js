6e818c9268f192f81ac1bc95a8d4e6f1c5f11213
hyperdrew21 <angryfiredragon@gmail.com>
2021-08-11 03:18:49 -0400

    styled header and navbar

--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -6,7 +6,7 @@ class Header extends Component {
     render() {
         return(
             <div className='header'>
-                <img src="http://via.placeholder.com/50x50"/>
+                <img className="header_img" src="http://via.placeholder.com/50x50"/>
                 <div className="header_links">
                 {
                     this.props.headerLinks.map((link, index) => {

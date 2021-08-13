c81bbca9f2c4d495ea702087699389ad685f6272
hyperdrew21 <angryfiredragon@gmail.com>
2021-08-13 18:55:04 -0400

    set up purchase history page

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class AccountInformation extends Component {
     render() {
         return (
-            <div> 
+            <div className= "account-information"> 
                 account info
             </div>
                 )

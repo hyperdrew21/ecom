74a221a93b71c59e9ab2f72b5340140d91c988cb
hyperdrew21 <angryfiredragon@gmail.com>
2021-08-13 19:18:37 -0400

    set up child components for purchase details

---
+++ src/components/account/purchaseDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PurchaseDetail extends Component {
+    render() {
+        const { className } = this.props;
+        return(        
+        <div className={`${className} purchase-detail`}>
+            purchase detail go here
+        </div>
+        )
+    }
+}
+export default PurchaseDetail;
--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
 import PageTitle from "../pageTitle";
+import Purchases from "./purchases";
+import PurchaseDetail from "./purchaseDetail"
 
 class PurchaseHistory extends Component {
     render() {
         return (
             <div className="purchase-history"> 
                 <PageTitle className= "purchase-history_page-title" title="Purchase history"/>
+                <Purchases className="purchase-history_purchases"/>
+                <PurchaseDetail className="purchase-history_detail"/>
+                <div className="purchase-history_git statusborder-bottom"></div>
             </div>
                 )
     }

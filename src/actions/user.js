1f936cac89864f53e015bb06f5c7ca2e9721d971
hyperdrew21 <angryfiredragon@gmail.com>
2021-08-17 11:49:35 -0400

    finished purchases content

---
+++ src/actions/user.js
@@ -0,0 +1,99 @@
+import { 
+    SET_USER_PURCHASES
+} from "./types";
+
+export function fetchUserPurchases() {
+    return ({
+        type: SET_USER_PURCHASES,
+        payload: [
+            {
+                _id: 0,
+                total: 8.02,
+                orderNumber: "34nuhbng43",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Jordan Hudgens",
+                    shippingAddress: "1234 West State"
+                }
+            },
+                {
+                _id: 1,
+                total: 19.20,
+                orderNumber: "34n56hy43",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Andrew Bristow",
+                    shippingAddress: "2510 North End"    
+                }
+            },
+                {
+                _id: 2,
+                total: 12.30,
+                orderNumber: "jmnughe43",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Rachel Murray",
+                    shippingAddress: "105 Gladys Ct"
+                }
+            },
+                {
+                _id: 3,
+                total: 8.20,
+                orderNumber: "3mu3e8hf",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Mille Deer",
+                    shippingAddress: "1537 Hawaiian Ave"
+                }
+            },
+                {
+                _id: 4,
+                total: 5.23,
+                orderNumber: "52415df",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Rebecca Green",
+                    shippingAddress: "4157 State"
+                }
+            },
+                {
+                _id: 5,
+                total: 7.65,
+                orderNumber: "jmugh743",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Jean Harris",
+                    shippingAddress: "New Orleans Drive"
+                }
+            },
+                {
+                _id: 6,
+                total: 9.50,
+                orderNumber: "33likof90",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Molly Kins",
+                    shippingAddress: "4517 Rolling Acres"
+                }
+            },
+                {
+                _id: 7,
+                total: 8.25,
+                orderNumber: "1254dlikd",
+                orderDate: new Date(),
+                creditCard: "-0000",
+                user: {
+                    name: "Dom King",
+                    shippingAddress: "4220 Flint Drive"
+                }
+            }
+        ]
+    })
+}
--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -4,16 +4,21 @@ import PageTitle from "../pageTitle";
 import Purchases from "./purchases";
 import PurchaseDetail from "./purchaseDetail"
 
+
+
 class PurchaseHistory extends Component {
     render() {
         return (
             <div className="purchase-history"> 
                 <PageTitle className= "purchase-history_page-title" title="Purchase history"/>
+                <div className="purchase-history_content">
                 <Purchases className="purchase-history_purchases"/>
                 <PurchaseDetail className="purchase-history_detail"/>
+                </div>
                 <div className="purchase-history_git statusborder-bottom"></div>
             </div>
                 )
     }
 }
+
 export default PurchaseHistory;

5568ab59530531145602fc17a7345196c3fe8bd5
hyperdrew21 <angryfiredragon@gmail.com>
2021-08-24 09:41:21 -0400

    made quantity and green tag components

---
+++ src/components/shop/shopProduct.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"; 
+import Quantity from "../quantity";
+import GreenPriceTag from "../greenPriceTag";
+
+class ShopProduct extends Component {
+    render() {
+        const { _id, title, description, price } = this.props;
+        return (
+            <div className="shop-product">
+                <div className="shop-product_title">
+                    {title}
+                </div>
+                <div className ="shop-product_description">
+                    {description}
+                </div>
+                <GreenPriceTag className="shop-product_price" title={price}/>
+                    
+                
+                <Quantity className="shop-product_quantity" quantity={1}/>
+                <div className="shop-product_add-to-cart">
+                    add to cart
+                </div>
+            </div>
+            )
+    }
+}
+
+export default ShopProduct;

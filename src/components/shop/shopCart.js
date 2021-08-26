98e48580f802a15af4392e1537275df6bf892fab
hyperdrew21 <angryfiredragon@gmail.com>
2021-08-26 09:38:31 -0400

    built out button for cart footer

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -8,12 +8,37 @@ function CartButton({className, icon}) {
         )
 }
 
-function CartContent({className}) {
+function CartContent({className, products}) {
+    let count = products.length
+    let productsJSX = products.map(product => <h1 key={product}> {product}</h1>);
     return (
         <div className={`${className} cart-content`}>
-
+            <div className="cart-content_title">
+                Cart ({count})
+            </div>
+            <div className="cart-content_product">
+                {productsJSX}
+            </div>
+            <CartFooter className="cart-footer_footer" products={products} />
         </div>
         )
+}
+
+function CartFooter({ className, products}) {
+    const price = 7.96;
+    return(
+        <div className={`${className} cart-footer`}>
+            <a className="cart-footer_checkout">
+                Checkout
+            </a>
+            <div className="cart-footer_subtotal">
+                Subtotal
+            </div>
+            <div className="cart-footer_price">
+                    ${price}
+            </div>
+        </div>
+    )
 }
 
 class ShopCart extends Component {
@@ -22,7 +47,7 @@ class ShopCart extends Component {
         return(
             <div className={`${className} shop-cart`}>
                 <CartButton className="shop-cart_toggle" icon="fas fa-times"/>
-                <CartContent className="shop-cart_content"/>
+                <CartContent className="shop-cart_content" products={[4]}/>
             </div>
             )
     }
